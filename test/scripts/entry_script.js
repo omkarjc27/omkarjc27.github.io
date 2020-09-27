a67358d392e4a8ba95e2f06f33870f97b72cacbe
Omkar Chalke <omkarchalke@protonmail.com>
2020-09-27 18:56:36 +0530

    error condn

--- test/scripts/entry_script.js
+++ test/scripts/entry_script.js
@@ -20,6 +20,8 @@ function SignUp() {
 				e_disp.innerHTML = "Username already taken."
 			} else if (data==true) {
 				console.log("Signed Up")
+			} else {
+				console.log("Signed Up Failed")
 			}
 		} else {
 			console.log("We connected to the server, but it returned an error.");
@@ -37,7 +39,6 @@ function CheckMail(inp) {
 	ourRequest.onload = function() {
 		if (ourRequest.status >= 200 && ourRequest.status < 400) {
 			var data = JSON.parse(ourRequest.responseText);
-			console.log(data)
 			if (data==false){
 				e_disp.innerHTML = "Email Address already in use."
 			} else {
@@ -58,11 +59,39 @@ function CheckUname(inp) {
 	ourRequest.onload = function() {
 		if (ourRequest.status >= 200 && ourRequest.status < 400) {
 			var data = JSON.parse(ourRequest.responseText);
-			console.log(data)
 			if (data==false){
 				e_disp.innerHTML = "Username already taken."
 			} else {
 				e_disp.innerHTML = ""
+			}
+		} else {
+			console.log("We connected to the server, but it returned an error.");
+		}
+	}
+}
+
+function Login() {
+	var form = document.getElementById("signup").getElementsByClassName("text-ip")
+	var email = form[0].value
+	var u_name = form[1].value
+	var password = form[2].value
+
+	var ourRequest = new XMLHttpRequest();
+	ourRequest.open('POST', 'https://api-snowglobe.herokuapp.com/signup/');
+	ourRequest.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
+	ourRequest.send(JSON.stringify({ "email": email, "u_name": u_name, "pass": password}));
+	ourRequest.onload = function() {
+		if (ourRequest.status >= 200 && ourRequest.status < 400) {
+			console.log(data)
+			var data = JSON.parse(ourRequest.responseText);
+			if (data=="E"){
+				var e_disp = document.getElementById("mail_e")
+				e_disp.innerHTML = "Email Address already in use."
+			} else if (data=="U") {
+				var e_disp = document.getElementById("name_e")
+				e_disp.innerHTML = "Username already taken."
+			} else if (data==true) {
+				console.log("Signed Up")
 			}
 		} else {
 			console.log("We connected to the server, but it returned an error.");
